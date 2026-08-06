2310c827fb391f0f43e94e8f71cbf6099804d3cb
Mike <madamovych@gmail.com>
2026-08-06 22:06:24 +0300

    Improved objects.ToString

--- util/objects/objects.go
+++ util/objects/objects.go
@@ -11,26 +11,46 @@ import (
 )
 
 func ToString(o any) string {
-	switch v := o.(type) {
-	case string:
-		return v
-	case []byte:
-		return string(v)
-	case int:
-		return strconv.Itoa(v)
-	case int64:
-		return strconv.FormatInt(v, 10)
-	case uint64:
-		return strconv.FormatUint(v, 10)
-	case float64:
-		return strconv.FormatFloat(v, 'f', -1, 64)
-	case float32:
-		return strconv.FormatFloat(float64(v), 'f', -1, 32)
-	case bool:
-		return strconv.FormatBool(v)
-	default:
-		return fmt.Sprintf("%v", o)
-	}
+    switch v := o.(type) {
+    case nil:
+        return "nil"
+    case string:
+        return v
+    case []byte:
+        return string(v)
+    case bool:
+        return strconv.FormatBool(v)
+    case int:
+        return strconv.Itoa(v)
+    case int8:
+        return strconv.FormatInt(int64(v), 10)
+    case int16:
+        return strconv.FormatInt(int64(v), 10)
+    case int32:
+        return strconv.FormatInt(int64(v), 10)
+    case int64:
+        return strconv.FormatInt(v, 10)
+    case uint:
+        return strconv.FormatUint(uint64(v), 10)
+    case uint8:
+        return strconv.FormatUint(uint64(v), 10)
+    case uint16:
+        return strconv.FormatUint(uint64(v), 10)
+    case uint32:
+        return strconv.FormatUint(uint64(v), 10)
+    case uint64:
+        return strconv.FormatUint(v, 10)
+    case float32:
+        return strconv.FormatFloat(float64(v), 'f', -1, 32)
+    case float64:
+        return strconv.FormatFloat(v, 'f', -1, 64)
+    case fmt.Stringer:
+        return v.String()
+    case error:
+        return v.Error()
+    default:
+        return fmt.Sprint(o)
+    }
 }
 
 func Equal(o1, o2 any) bool {
--- util/objects/objects_test.go
+++ util/objects/objects_test.go
@@ -1,6 +1,7 @@
 package objects_test
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -10,90 +11,93 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestToString(t *testing.T) {
-	t.Run("should produce sane results'", func(t *testing.T) {
-		require.Equal(t, "123", objects.ToString(int(123)))
-		require.Equal(t, "123", objects.ToString(int8(123)))
-		require.Equal(t, "123", objects.ToString(int16(123)))
-		require.Equal(t, "123", objects.ToString(int32(123)))
-		require.Equal(t, "123", objects.ToString(int64(123)))
-		require.Equal(t, "123", objects.ToString(uint(123)))
-		require.Equal(t, "123", objects.ToString(uint8(123)))
-		require.Equal(t, "123", objects.ToString(uint16(123)))
-		require.Equal(t, "123", objects.ToString(uint32(123)))
-		require.Equal(t, "123", objects.ToString(uint64(123)))
-		require.Equal(t, "123", objects.ToString(float32(123)))
-		require.Equal(t, "123", objects.ToString(float64(123)))
-		require.Equal(t, "123", objects.ToString("123"))
-		type Port int8
-		require.Equal(t, "123", objects.ToString(Port(123)))
-		require.Equal(t, "true", objects.ToString(true))
-	})
+func TestToString(test *testing.T) {
+	require.Equal(test, "nil", objects.ToString(nil))
+	require.Equal(test, "abc", objects.ToString("abc"))
+	require.Equal(test, "abc", objects.ToString([]byte("abc")))
+
+	require.Equal(test, "true", objects.ToString(true))
+	require.Equal(test, "false", objects.ToString(false))
+
+	require.Equal(test, "1", objects.ToString(int(1)))
+	require.Equal(test, "2", objects.ToString(int8(2)))
+	require.Equal(test, "3", objects.ToString(int16(3)))
+	require.Equal(test, "4", objects.ToString(int32(4)))
+	require.Equal(test, "5", objects.ToString(int64(5)))
+
+	require.Equal(test, "6", objects.ToString(uint(6)))
+	require.Equal(test, "7", objects.ToString(uint8(7)))
+	require.Equal(test, "8", objects.ToString(uint16(8)))
+	require.Equal(test, "9", objects.ToString(uint32(9)))
+	require.Equal(test, "10", objects.ToString(uint64(10)))
+
+	require.Equal(test, "1.5", objects.ToString(float32(1.5)))
+	require.Equal(test, "2.5", objects.ToString(float64(2.5)))
+
+	require.Equal(test, "boom", objects.ToString(errors.New("boom")))
+
+	require.Equal(test, "[1 2 3]", objects.ToString([]int{1, 2, 3}))
 }
 
 func Test_Equal(t *testing.T) {
-	t.Run("should produce expected results'", func(t *testing.T) {
-		// Equal
-		require.True(t, objects.Equal(255, 255))
+	// Equal
+	require.True(t, objects.Equal(255, 255))
 
-		time1 := time.Now()
-		time2 := time1
-		require.True(t, objects.Equal(time1, time2))
+	time1 := time.Now()
+	time2 := time1
+	require.True(t, objects.Equal(time1, time2))
 
-		slice1 := make([]any, 0)
-		slice2 := slice1
-		require.True(t, objects.Equal(slice1, slice2))
+	slice1 := make([]any, 0)
+	slice2 := slice1
+	require.True(t, objects.Equal(slice1, slice2))
 
-		require.True(t, objects.Equal(slice1, make([]any, 0)))
+	require.True(t, objects.Equal(slice1, make([]any, 0)))
 
-		// Not eqal
-		require.False(t, objects.Equal(255, 256))
+	// Not eqal
+	require.False(t, objects.Equal(255, 256))
 
-		slice1 = make([]any, 0)
-		slice2 = append(slice1, 1)
-		require.False(t, objects.Equal(slice1, slice2))
-	})
+	slice1 = make([]any, 0)
+	slice2 = append(slice1, 1)
+	require.False(t, objects.Equal(slice1, slice2))
 }
 
 func Test_HashCode(t *testing.T) {
-	t.Run("should produce expected results'", func(t *testing.T) {
-		require.Equal(t, 8114084241814008568, objects.HashCode(int(123)))
-		require.Equal(t, 4953291999513788008, objects.HashCode(int8(123)))
-		require.Equal(t, -7208379536669766984, objects.HashCode(int16(123)))
-		require.Equal(t, 5684921998043602808, objects.HashCode(int32(123)))
-		require.Equal(t, 8114084241814008568, objects.HashCode(int64(123)))
-		require.Equal(t, 8114084241814008568, objects.HashCode(uint(123)))
-		require.Equal(t, 4953291999513788008, objects.HashCode(uint8(123)))
-		require.Equal(t, -7208379536669766984, objects.HashCode(uint16(123)))
-		require.Equal(t, 5684921998043602808, objects.HashCode(uint32(123)))
-		require.Equal(t, 8114084241814008568, objects.HashCode(uint64(123)))
-		require.Equal(t, 3326946953198266991, objects.HashCode(float32(123)))
-		require.Equal(t, 2082925262096509245, objects.HashCode(float64(123)))
-		require.Equal(t, 4591456448132287629, objects.HashCode("123"))
-		type Port int8
-		require.Equal(t, 4953291999513788008, objects.HashCode(Port(123)))
-		require.Equal(t, 4953162257141659110, objects.HashCode(true))
+	require.Equal(t, 8114084241814008568, objects.HashCode(int(123)))
+	require.Equal(t, 4953291999513788008, objects.HashCode(int8(123)))
+	require.Equal(t, -7208379536669766984, objects.HashCode(int16(123)))
+	require.Equal(t, 5684921998043602808, objects.HashCode(int32(123)))
+	require.Equal(t, 8114084241814008568, objects.HashCode(int64(123)))
+	require.Equal(t, 8114084241814008568, objects.HashCode(uint(123)))
+	require.Equal(t, 4953291999513788008, objects.HashCode(uint8(123)))
+	require.Equal(t, -7208379536669766984, objects.HashCode(uint16(123)))
+	require.Equal(t, 5684921998043602808, objects.HashCode(uint32(123)))
+	require.Equal(t, 8114084241814008568, objects.HashCode(uint64(123)))
+	require.Equal(t, 3326946953198266991, objects.HashCode(float32(123)))
+	require.Equal(t, 2082925262096509245, objects.HashCode(float64(123)))
+	require.Equal(t, 4591456448132287629, objects.HashCode("123"))
+	type Port int8
+	require.Equal(t, 4953291999513788008, objects.HashCode(Port(123)))
+	require.Equal(t, 4953162257141659110, objects.HashCode(true))
 
-		// Equal
-		require.Equal(t, objects.HashCode(255), objects.HashCode(255))
+	// Equal
+	require.Equal(t, objects.HashCode(255), objects.HashCode(255))
 
-		time1 := time.Now()
-		time2 := time1
-		require.Equal(t, objects.HashCode(time1), objects.HashCode(time2))
+	time1 := time.Now()
+	time2 := time1
+	require.Equal(t, objects.HashCode(time1), objects.HashCode(time2))
 
-		slice1 := make([]any, 0)
-		slice2 := slice1
-		require.Equal(t, objects.HashCode(slice1), objects.HashCode(slice2))
+	slice1 := make([]any, 0)
+	slice2 := slice1
+	require.Equal(t, objects.HashCode(slice1), objects.HashCode(slice2))
 
-		require.Equal(t, objects.HashCode(slice1), objects.HashCode(make([]any, 0)))
+	require.Equal(t, objects.HashCode(slice1), objects.HashCode(make([]any, 0)))
 
-		// Not eqal
-		require.NotEqual(t, objects.HashCode(255), objects.HashCode(256))
+	// Not eqal
+	require.NotEqual(t, objects.HashCode(255), objects.HashCode(256))
 
-		slice1 = make([]any, 0)
-		slice2 = append(slice1, 1)
-		require.NotEqual(t, objects.HashCode(slice1), objects.HashCode(slice2))
-	})
+	slice1 = make([]any, 0)
+	slice2 = append(slice1, 1)
+	require.NotEqual(t, objects.HashCode(slice1), objects.HashCode(slice2))
 }
 
 func TestFirstNonNil(t *testing.T) {
