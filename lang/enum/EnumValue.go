d6388db9af2b2ba23f2e936e589925fe219193a4
Mike <madamovych@gmail.com>
2026-08-08 17:30:28 +0300

    Improved Enum

--- lang/enum/EnumValue.go
+++ lang/enum/EnumValue.go
@@ -12,36 +12,51 @@ func ValuesOf[T any](d *Enum[T]) []T {
 	return d.Values()
 }
 
+type enumValue[T any] struct {
+	enum       *Enum[T]
+	name       string
+	ordinal    int
+	properties []any
+}
+
 type EnumValue[T any] struct {
-	enum    *Enum[T]
-	name    string
-	ordinal int
+	value *enumValue[T]
 }
 
-func NewEnumValue[T any](name string) *EnumValue[T] {
-	return &EnumValue[T]{name: name, ordinal: -1}
+func NewEnumValue[T any](name string, properties ...any) EnumValue[T] {
+	return EnumValue[T]{
+		value: &enumValue[T]{
+			name:       name,
+			ordinal:    -1,
+			properties: properties,
+		},
+	}
 }
 
-func (this *EnumValue[T]) Name() string {
-	return this.name
+func (this EnumValue[T]) Name() string {
+	return this.value.name
 }
 
-func (this *EnumValue[T]) Ordinal() int {
-	return this.ordinal
+func (this EnumValue[T]) Ordinal() int {
+	return this.value.ordinal
 }
 
-func (this *EnumValue[T]) Enum() *Enum[T] {
-	return this.enum
+func (this EnumValue[T]) Enum() *Enum[T] {
+	return this.value.enum
 }
 
-func (this *EnumValue[T]) String() string {
-	return this.name
+func (this EnumValue[T]) Property(index int) any {
+	return this.value.properties[index]
 }
 
-func (this *EnumValue[T]) setOrdinal(ordinal int) {
-	this.ordinal = ordinal
+func (this EnumValue[T]) String() string {
+	return this.value.name
 }
 
-func (this *EnumValue[T]) setDescriptor(descriptor *Enum[T]) {
-	this.enum = descriptor
+func (this EnumValue[T]) setOrdinal(ordinal int) {
+	this.value.ordinal = ordinal
+}
+
+func (this EnumValue[T]) setDescriptor(descriptor *Enum[T]) {
+	this.value.enum = descriptor
 }
--- lang/enum/Enum_test.go
+++ lang/enum/Enum_test.go
@@ -1,6 +1,7 @@
 package enum_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/go-jang/go/lang/enum"
@@ -8,30 +9,47 @@ import (
 )
 
 type Color struct {
-	enum.EnumValue[*Color]
+	enum.EnumValue[Color]
 }
 
-func NewColor(name string) *Color {
-	return &Color{EnumValue: *enum.NewEnumValue[*Color](name)}
+func NewColor(name, description string) Color {
+	return Color{EnumValue: enum.NewEnumValue[Color](name, description)}
+}
+
+func (this Color) Description() string {
+	return this.Property(0).(string)
+}
+
+func (this Color) MarshalJSON() ([]byte, error) {
+	return json.Marshal(this.Name())
+}
+
+func (this *Color) UnmarshalJSON(data []byte) error {
+	var name string
+	if err := json.Unmarshal(data, &name); err != nil {
+		return err
+	}
+	*this = RGB.ValueOf(name)
+	return nil
 }
 
 var RGB = enum.NewEnum(
-	NewColor("Red"),
-	NewColor("Green"),
-	NewColor("Blue"),
+	NewColor("Red", "Description of red"),
+	NewColor("Green", "Description of green"),
+	NewColor("Blue", "Description of blue"),
 )
 
 var RGB2 = enum.NewEnum(
-	NewColor("Red"),
-	NewColor("Green"),
-	NewColor("Blue"),
+	NewColor("Red", "Description of red"),
+	NewColor("Green", "Description of green"),
+	NewColor("Blue", "Description of blue"),
 )
 
 var NotRGB = enum.NewEnum(
-	NewColor("Red"),
-	NewColor("Green"),
-	NewColor("Blue"),
-	NewColor("Black"),
+	NewColor("Red", "Description of red"),
+	NewColor("Green", "Description of green"),
+	NewColor("Blue", "Description of blue"),
+	NewColor("Black", "Description of black"),
 )
 
 func TestEnumDescriptor_ValueOfOrdinal(t *testing.T) {
@@ -42,13 +60,8 @@ func TestEnumDescriptor_ValueOfOrdinal(t *testing.T) {
 	})
 
 	t.Run("same instance returned", func(t *testing.T) {
-		require.Same(t, RGB.ValueOfOrdinal(0), RGB.ValueOf("Red"))
-		require.Same(t, RGB.ValueOfOrdinal(1), RGB.ValueOf("Green"))
-	})
-
-	t.Run("float discrete values", func(t *testing.T) {
-		require.Equal(t, "Red", RGB.ValueOfOrdinal(0).Name())
-		require.Equal(t, "Green", RGB.ValueOfOrdinal(1).Name())
+		require.True(t, RGB.ValueOfOrdinal(0) == RGB.ValueOf("Red"))
+		require.True(t, RGB.ValueOfOrdinal(1) == RGB.ValueOf("Green"))
 	})
 
 	t.Run("invalid ordinals", func(t *testing.T) {
@@ -64,18 +77,61 @@ func TestEnumDescriptor_ValueOfOrdinal(t *testing.T) {
 
 		red := blue.Enum().ValueOf("Red")
 		require.Equal(t, "Red", red.Name())
-		require.Same(t, RGB.ValueOf("Red"), red)
+		require.True(t, RGB.ValueOf("Red") == red)
 	})
 
-	t.Run("different types with the save value", func(t *testing.T) {
-		// same universe
+	t.Run("different types with the same value", func(t *testing.T) {
 		require.Equal(t, RGB.ValueOf("Red"), RGB2.ValueOf("Red"))
 		require.Equal(t, RGB.ValueOf("Red").Name(), RGB2.ValueOf("Red").Name())
-		require.Equal(t, RGB.ValueOf("Red").Ordinal(), RGB2.ValueOf("Red").Ordinal())
+		require.False(t, RGB.ValueOf("Red") == RGB2.ValueOf("Red"))
 
-		// different universe
 		require.NotEqual(t, RGB.ValueOf("Red"), NotRGB.ValueOf("Red"))
 		require.Equal(t, RGB.ValueOf("Red").Name(), NotRGB.ValueOf("Red").Name())
-		require.Equal(t, RGB.ValueOf("Red").Ordinal(), NotRGB.ValueOf("Red").Ordinal())
+		require.False(t, RGB.ValueOf("Red") == NotRGB.ValueOf("Red"))
 	})
+}
+
+func TestEnumJSON(t *testing.T) {
+	type Model struct {
+		Text  string `json:"text"`
+		Color Color  `json:"color"`
+	}
+
+	red := RGB.ValueOf("Red")
+	green := RGB.ValueOf("Green")
+	blue := RGB.ValueOf("Blue")
+
+	source := Model{Text: "test", Color: red}
+	data, err := json.Marshal(source)
+	require.NoError(t, err)
+	require.Equal(t, `{"text":"test","color":"Red"}`, string(data))
+	require.Equal(t, `Red`, red.Name())
+	require.Equal(t, 0, red.Ordinal())
+	require.Equal(t, `Description of red`, red.Description())
+
+	target := Model{Color: green}
+	err = json.Unmarshal(data, &target)
+	require.NoError(t, err)
+
+	require.Equal(t, "test", target.Text)
+	require.True(t, target.Color == red)
+	require.Equal(t, "Red", target.Color.Name())
+	require.Equal(t, 0, target.Color.Ordinal())
+	require.Equal(t, "Description of red", target.Color.Description())
+
+	require.True(t, RGB.ValueOf("Red") == red)
+	require.True(t, RGB.ValueOf("Green") == green)
+	require.True(t, RGB.ValueOf("Blue") == blue)
+
+	require.Equal(t, "Red", RGB.ValueOf("Red").Name())
+	require.Equal(t, "Green", RGB.ValueOf("Green").Name())
+	require.Equal(t, "Blue", RGB.ValueOf("Blue").Name())
+
+	require.Equal(t, 0, RGB.ValueOf("Red").Ordinal())
+	require.Equal(t, 1, RGB.ValueOf("Green").Ordinal())
+	require.Equal(t, 2, RGB.ValueOf("Blue").Ordinal())
+
+	require.Equal(t, "Description of red", RGB.ValueOf("Red").Description())
+	require.Equal(t, "Description of green", RGB.ValueOf("Green").Description())
+	require.Equal(t, "Description of blue", RGB.ValueOf("Blue").Description())
 }
